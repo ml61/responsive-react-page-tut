63e229fa13b74344b75889a9923759b9737d437b
Maksym Lysenko <mlysenko0601@gmail.com>
2021-04-29 21:10:59 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./components/styles/App.css";
+import Services from "./components/pages/Services/Services";
+import Products from "./components/pages/Products/Products";
+import SignUp from "./components/pages/SignUp/SignUp";
 import Navbar from "./components/Navbar";
 import Home from "./components/pages/HomePage/Home";
+import Footer from "./components/pages/Footer/Footer";
 
 function App() {
   return (
@@ -9,7 +13,11 @@ function App() {
       <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/services" exact component={Services} />
+        <Route path="/products" exact component={Products} />
+        <Route path="/sign-up" exact component={SignUp} />
       </Switch>
+      <Footer />
     </Router>
   );
 }
